91edb2a3e2a7ead135323031d6ac1586a6788625
Ray Lin <raylincontact@icloud.com>
2023-03-16 12:30:34 +0800

    feat: Create DocxFile struct
    Create Types based on un-zipped docx file

---
+++ docx.go
@@ -0,0 +1,41 @@
+package godocxkit
+
+import "archive/zip"
+
+type DocxFile struct {
+	RelsDirectory DocxRelsDirectory 
+	PropsDirectory DocxPropsDirectory
+	WordDirectory DocxWordDirectory
+	ContentTypesXml DocxContentTypesXml
+}
+
+type DocxContentTypesXml *zip.File
+
+type DocxWordDirectory struct {
+	DocumentXml *zip.File
+	EndNotesXml *zip.File
+	FontTableXml *zip.File
+	FootNotesXml *zip.File
+	NumberingXml *zip.File
+	SettingsXml *zip.File
+	StylesXml *zip.File
+	WebSettingsXml *zip.File
+}
+
+type DocxWordRelsDirectory struct {
+	DocumentXmlRels *zip.File
+}
+
+type DocxWordThemeDirectory struct {
+	Theme1Xml *zip.File
+}
+
+type DocxPropsDirectory struct {
+	AppXml *zip.File
+	CoreXml *zip.File
+	CustomXml *zip.File
+}
+
+type DocxRelsDirectory struct {
+	Rels *zip.File
+}
