454cd33ca4fe3382b5008539ac5eaef1c4ea7d06
Ray Lin <raylincontact@icloud.com>
2023-03-16 17:03:14 +0800

    test: Add template generation test

---
+++ kit/template/template-kit_test.go
@@ -0,0 +1,46 @@
+package template
+
+import (
+	"go-docx-kit/docx"
+	"os"
+	"testing"
+)
+
+func TestTemplateGeneration(t *testing.T) {
+	dataModel := NewTemplateModel()
+	dataModel.Put("receiver", "RD Team")
+	dataModel.Put("refNumber", "40666888")
+	dataModel.Put("reporterName", "Kent Chou")
+	dataModel.Put("reporterRank", "Intern")
+	dataModel.Put("departmentName", "QA")
+	dataModel.Put("telephone", "0911222333")
+	dataModel.Put("reportDate", "2023/03/15")
+	dataModel.Put("productName", "Pencil X")
+	dataModel.Put("manufacturer", "Tesla")
+	dataModel.Put("batchNumber", "777")
+	dataModel.Put("quantity", "900")
+	dataModel.Put("natureOfComplain", "The pencil is too short.")
+	dataModel.Put("isSampleRetained", "■")
+	dataModel.Put("noSampleRetained", "□")
+	dataModel.Put("retainedCount", "900")
+	dataModel.Put("reasonOfNotRetainingSample", " ")
+
+	templateFile, err := docx.ReadDocxFile("../../template.docx")
+	if err != nil {
+		panic(err)
+	}
+
+	resultFile, err := os.Create("../../result.docx")
+	if err != nil {
+		panic(err)
+	}
+	defer resultFile.Close()
+
+	kit := NewTemplateKit()
+	kit.SetTargetDocx(templateFile)
+	kit.SetTemplateModel(dataModel)
+	_, err = kit.Generate(resultFile)
+	if err != nil {
+		panic(err)
+	}
+}
