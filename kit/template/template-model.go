8793c380955cff6198e7226d5a6bef0c9e027bcf
Ray Lin <raylincontact@icloud.com>
2023-03-16 14:44:49 +0800

    feat: Add TemplateModel
    TemplateModel is basically a HashMap.
    The GetTrie() will be used to detect template value when parsing document.xml

---
+++ kit/template/template-model.go
@@ -0,0 +1,41 @@
+package template
+
+type TemplateModel struct {
+	hashMap map[string]string
+}
+
+func NewTemplateModel() *TemplateModel {
+	return new(TemplateModel)
+}
+
+func (t *TemplateModel) Put(key string, value string) {
+	t.hashMap[key] = value
+}
+
+func (t *TemplateModel) Get(key string) string {
+	return t.hashMap[key]
+}
+
+func (t *TemplateModel) GetTrie() *CharacterToken {
+	rootToken := NewCharacterTokenTrie()
+
+	for key, value := range t.hashMap {
+		t.insertTrie(rootToken, key, value)
+	}
+
+	return rootToken
+}
+
+func (t *TemplateModel) insertTrie(token *CharacterToken, key, value string) {
+	if len(key) == 0 {
+		token.Value = []byte(value)
+		return
+	}
+
+	currentByte := key[0]
+	if _, ok := token.NextCharacterTokens[currentByte]; !ok {
+		token.NextCharacterTokens[currentByte] = NewCharacterToken()
+	}
+
+	t.insertTrie(token.NextCharacterTokens[currentByte], key[1:], value)
+}
